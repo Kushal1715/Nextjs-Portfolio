d3bf7ef380839f2b42f097db86c828915ecb606e
Kushal1715 <kushalkhadka77@gmail.com>
2025-07-23 15:51:30 +0545

    about me completed

--- portfolio/src/components/home/About.tsx
+++ portfolio/src/components/home/About.tsx
@@ -10,32 +10,39 @@ const About = () => {
       <h1 className="text-5xl">About me</h1>
       <div className="flex gap-10 mt-12">
         <div className="">
-          <img src="/myImage.jpg" className="h-[380px] w-[320px] rounded-2xl" />
+          <img src="/myImage.jpg" className="h-[350px] w-[320px] rounded-2xl" />
         </div>
-        <div className="flex flex-col space-y-8">
-          <p className="text-left">
-            I am an experienced Frontend Developer with over a decade of
-            professional expertise in <br /> the field. Throughout my career, I
-            have had the privilege of collaborating with prestigious <br />
-            organizations, contributing to their success and growth.
+        <div className="flex flex-col space-y-8 py-4">
+          <p className="text-left text-lg">
+            I am an experienced Full Stack Developer with over a decade of
+            <br />
+            professional expertise in the field. Throughout my career, I have
+            <br />
+            had the privilege of collaborating with prestigious organizations,
+            <br />
+            contributing to their success and growth.
           </p>
           <div className="flex gap-4">
-            <div className="flex flex-col text-left border-2 p-4 rounded-lg space-y-2">
+            <div className="flex flex-col text-left border-2 p-4 rounded-lg space-y-2 w-[185px]">
+              <img src="/about/about2.png" className="h-6 w-6 mb-6" />
               <h3 className="font-semibold text-[20px]">Languages</h3>
               <p className="text-[15px] text-gray-500">
-                HTML, CSS, JavaScript <br /> React Js, Next Js
+                JavaScript, React Js, Next Js, Express Js
               </p>
             </div>
-            <div className="flex flex-col text-left border-2 p-4 rounded-lg space-y-2">
-              <h3 className="font-semibold text-[20px]">Languages</h3>
+            <div className="flex flex-col text-left border-2 p-4 rounded-lg space-y-2 w-[185px]">
+              <img src="/about/about3.png" className="h-6 w-6 mb-6" />
+              <h3 className="font-semibold text-[20px]">Education</h3>
               <p className="text-[15px] text-gray-500">
-                HTML, CSS, JavaScript <br /> React Js, Next Js
+                Bachelor of Computer
+                <br /> Application
               </p>
             </div>
-            <div className="flex flex-col text-left border-2 p-4 rounded-lg space-y-2">
-              <h3 className="font-semibold text-[20px]">Languages</h3>
+            <div className="flex flex-col text-left border-2 p-4 rounded-lg space-y-2 w-[185px]">
+              <img src="/about/about1.png" className="h-6 w-6 mb-6" />
+              <h3 className="font-semibold text-[20px]">Projects</h3>
               <p className="text-[15px] text-gray-500">
-                HTML, CSS, JavaScript <br /> React Js, Next Js
+                Built more than 5 <br /> projects
               </p>
             </div>
           </div>
